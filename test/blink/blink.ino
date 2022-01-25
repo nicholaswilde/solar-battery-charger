17c4197d87e579c0e22446ca4ca162b57bdcd247
nιcнolaѕ wιlde <ncwilde43@gmail.com>
2022-01-25 14:22:38 -0800

    Update blink.ino

--- test/blink/blink.ino
+++ test/blink/blink.ino
@@ -1,10 +1,24 @@
+/*--------------------------------------------------------------
+  Program:      blink
+  
+  Description:  Blnink the built in LED.
+  
+  Hardware:     Adafruit Feather Huzzah
+                
+  Software:     Developed using arduino-cli 0.20.2.
+  
+  Date:         24JAN2022
+ 
+  Author:       Nicholas Wilde 0x08b7d7a3
+--------------------------------------------------------------*/
+
 void setup() {
-    pinMode(LED_BUILTIN, OUTPUT);
+  pinMode(LED_BUILTIN, OUTPUT);
 }
  
 void loop() {
-    digitalWrite(LED_BUILTIN, HIGH);
-    delay(1000);
-    digitalWrite(LED_BUILTIN, LOW);
-    delay(1000);
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(1000);
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(1000);
 }
