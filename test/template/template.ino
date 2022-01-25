120876a9b370e5018a597895e2947c91e4d8a5fe
nιcнolaѕ wιlde <ncwilde43@gmail.com>
2022-01-25 09:30:56 -0800

    Update template.ino

--- test/template/template.ino
+++ test/template/template.ino
@@ -1,1 +1,15 @@
+/*--------------------------------------------------------------
+  Program:      template
 
+  Description:  Reads value on analog input A0 and calculates
+                the the percent level of a lipo battery.
+  
+  Hardware:     Adafruit Feather Huzzah with 3.7V lipo battery
+                and voltage divider on A0.
+                
+  Software:     Developed using arduino-cli 0.20.2.
+
+  Date:         24JAN2022
+ 
+  Author:       Nicholas Wilde 0x08b7d7a3
+--------------------------------------------------------------*/
