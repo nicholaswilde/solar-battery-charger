da73605bc257249919f6071b12823adb7ead0516
nιcнolaѕ wιlde <ncwilde43@gmail.com>
2022-01-25 09:30:04 -0800

    Update battery.ino

--- test/battery/battery.ino
+++ test/battery/battery.ino
@@ -4,15 +4,14 @@
   Description:  Reads value on analog input A0 and calculates
                 the the percent level of a lipo battery.
   
-  Hardware:     Adafruit Feather Huzzah with voltage divider on
-                A0.
+  Hardware:     Adafruit Feather Huzzah with 3.7V lipo battery
+                and voltage divider on A0.
                 
-  Software:     Developed using Arduino 1.0.5 software
-                Should be compatible with Arduino 1.0 +
+  Software:     Developed using arduino-cli 0.20.2.
 
   Date:         24JAN2022
  
-  Author:       Nicholas Wilde
+  Author:       Nicholas Wilde 0x08b7d7a3
 --------------------------------------------------------------*/
 
 // number of analog samples to take per reading
