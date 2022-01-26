48d92d264bc99f5403281baa10b8e953c8d917d1
nιcнolaѕ wιlde <ncwilde43@gmail.com>
2022-01-26 07:42:48 -0800

    Add sketch.json

--- solar-battery-charger.ino
+++ solar-battery-charger.ino
@@ -1,19 +1,21 @@
 /*--------------------------------------------------------------
   Program:      solar-battery-charger
-  
-  Description:  Reports the battery level of the charged li-ion
-                battery and notifies when the battery is
-                finished charging
-  
-  Hardware:     Adafruit Feather Huzzah, TP4056, 5V boost board,
-                18650 li-ion batteries, 7.5V 0.5A solar panel
-                
-  Software:     Developed using arduino-cli 0.20.2.
-  
-  Date:         24JAN2022
- 
+
+  Description:  Reads value on analog input A0 and calculates
+                the the percent level of a lipo battery.
+
+  Hardware:     Adafruit Feather Huzzah with 3.7V lipo battery
+                and voltage divider on A0.
+
+  Software:     Developed using arduino-cli ${VERSION}.
+
+  Date:         26JAN2022
+
   Author:       Nicholas Wilde 0x08b7d7a3
 --------------------------------------------------------------*/
+
+#include <ESP8266WiFi.h>
+#include "secrets.h"
 
 #define BAUD_RATE 115200 // baud rate used for Serial console
 #define DELAY_LOOP 1000  // loop delay time (ms)
