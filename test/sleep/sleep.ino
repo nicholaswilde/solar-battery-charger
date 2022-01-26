f0840cf8dff503024764c4ae664fa0ef621a681c
nιcнolaѕ wιlde <ncwilde43@gmail.com>
2022-01-25 21:32:37 -0800

    Update sleep.ino

--- test/sleep/sleep.ino
+++ test/sleep/sleep.ino
@@ -13,7 +13,7 @@
   Author:       Nicholas Wilde 0x08b7d7a3
 --------------------------------------------------------------*/
 
-#define BAUD_RATE 9600 // baud rate used for Serial console
+#define BAUD_RATE 115200 // baud rate used for Serial console
 #define SLEEP_TIME 10    // sleep time (s)
 #define DELAY_LOOP 5000  // loop delay time (ms)
 
@@ -24,8 +24,6 @@ void setup() {
 
 void loop() {
   Serial.println("waking up");
-  // digitalWrite(LED_BUILTIN, HIGH);
-  // delay(DELAY_LOOP);
   digitalWrite(LED_BUILTIN, LOW);
   delay(DELAY_LOOP);
   Serial.println("going to sleep");
