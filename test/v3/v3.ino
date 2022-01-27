b8fc16382363bce0b86b00766025fc132c5a193a
nιcнolaѕ wιlde <ncwilde43@gmail.com>
2022-01-27 01:15:25 +0000

    Add v3

---
+++ test/v3/v3.ino
@@ -0,0 +1,30 @@
+/*--------------------------------------------------------------
+  Program:      ${TESTNAME}
+
+  Description:  Reads value on analog input A0 and calculates
+                the the percent level of a lipo battery.
+  
+  Hardware:     Adafruit Feather Huzzah with 3.7V lipo battery
+                and voltage divider on A0.
+                
+  Software:     Developed using arduino-cli ${VERSION}.
+
+  Date:         ${DATE}
+ 
+  Author:       Nicholas Wilde 0x08b7d7a3
+--------------------------------------------------------------*/
+
+#include <ESP8266WiFi.h>
+#include "secrets.h"
+
+#define BAUD_RATE 115200 // baud rate used for Serial console
+#define DELAY_LOOP 1000  // loop delay time (ms)
+
+void setup() {
+  Serial.begin(BAUD_RATE);
+}
+
+void loop() {
+  Serial.println("test");
+  delay(DELAY_LOOP);
+}
