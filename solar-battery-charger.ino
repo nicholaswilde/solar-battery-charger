9dd51e36ac917b60ab754775c05f619a36703452
nιcнolaѕ wιlde <ncwilde43@gmail.com>
2022-01-29 20:35:59 -0800

    Break out functions

--- solar-battery-charger.ino
+++ solar-battery-charger.ino
@@ -1,5 +1,5 @@
 /*--------------------------------------------------------------
-  Program:      thingspeak
+  Program:      solar-battery-charger
 
   Description:  Reads value on analog input A0 and calculates
                 the the percent level of a lipo battery and
@@ -26,8 +26,9 @@
 #define R2 220000        // resistor 2 on the voltage divider
 #define VOLTAGE_MAX 4.2  // max voltage of lipo battery
 #define VOLTAGE_MIN 3.14 // min voltage of lipo battery
-#define DELAY_LOOP 20000  // delay for loop routine
+#define DELAY_LOOP 20000 // delay for loop routine
 #define DELAY_SAMPLE 10  // delay between samples
+#define SLEEP_TIME 60    // sleep time (s)
 
 WiFiClient  client;
 
@@ -38,7 +39,6 @@ unsigned long myChannelNumber = SECRET_CH_ID;
 const char * myWriteAPIKey = SECRET_WRITE_APIKEY;
 
 int number = 0;
-
 int sum = 0;                    // sum of samples taken
 unsigned char sample_count = 0; // current sample number
 int level = 0;                  // calculated battery level
@@ -55,6 +55,7 @@ void setup() {
   // calculate the min battery number using the resistor ratio
   battery_min=(float)resistor_ratio*(float)VOLTAGE_MIN*1024;
   // battery_min=580;
+
   // calculate the max battery number using the resistor ratio
   battery_max=(float)resistor_ratio*(float)VOLTAGE_MAX*1024;
   // battery_max=774;
@@ -63,23 +64,28 @@ void setup() {
 
   WiFi.mode(WIFI_STA);
   ThingSpeak.begin(client);  // Initialize ThingSpeak
+  Serial.println();
 }
 
 void loop() {
-  // take a number of analog samples and add them up
-  while (sample_count < NUM_SAMPLES) {
-    sum += analogRead(ANALOG_PIN_NO);
-    sample_count++;
-    delay(DELAY_SAMPLE);
-  }
   digitalWrite(LED_BUILTIN, LOW);
-  level = (float)sum / (float)NUM_SAMPLES;
-  // convert battery level to percent
-  level = map(level, battery_min, battery_max, 0, 100);
-  Serial.print("Battery level: ");
-  Serial.print(level);
-  Serial.println("%");
-    // Connect or reconnect to WiFi
+
+  conntectToWifi();
+
+  sum = getSum();
+
+  level = getBatteryLevel(sum);
+
+  writeToThingSpeak(level);
+
+  digitalWrite(LED_BUILTIN, HIGH);
+  //delay(DELAY_LOOP);
+  Serial.println("Going to sleep");
+  ESP.deepSleep(SLEEP_TIME * 1e6);
+}
+
+void conntectToWifi(){
+   // Connect or reconnect to WiFi
   if(WiFi.status() != WL_CONNECTED){
     Serial.print("Attempting to connect to SSID: ");
     Serial.println(SECRET_SSID);
@@ -89,18 +95,38 @@ void loop() {
       digitalWrite(LED_BUILTIN, !digitalRead(LED_BUILTIN));
       delay(5000);
     }
-    Serial.println("\nConnected.");
+    Serial.println("connected");
   }
+}
 
+int getSum(){
+   // take a number of analog samples and add them up
+  while (sample_count < NUM_SAMPLES) {
+    sum += analogRead(ANALOG_PIN_NO);
+    sample_count++;
+    delay(DELAY_SAMPLE);
+  }
+  return sum;
+}
+
+int getBatteryLevel(int sum){
+  level = (float)sum / (float)NUM_SAMPLES;
+  // convert battery level to percent
+  level = map(level, battery_min, battery_max, 0, 100);
+  Serial.print("Battery level: ");
+  Serial.print(level);
+  Serial.println("%");
+  return level;
+}
+
+void writeToThingSpeak(int level){
   // Write to ThingSpeak. There are up to 8 fields in a channel, allowing you to store up to 8 different
   // pieces of information in a channel.  Here, we write to field 1.
   int x = ThingSpeak.writeField(myChannelNumber, 1, level, myWriteAPIKey);
   if(x == 200){
-    Serial.println("Channel update successful.");
+    Serial.println("Channel update successful");
   }
   else{
     Serial.println("Problem updating channel. HTTP error code " + String(x));
   }
-  digitalWrite(LED_BUILTIN, HIGH);
-  delay(DELAY_LOOP);
 }
