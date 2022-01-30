116f5fe5da9283e389df011847935c2f11f2771d
nιcнolaѕ wιlde <ncwilde43@gmail.com>
2022-01-29 23:06:56 -0800

    Add second field

--- solar-battery-charger.ino
+++ solar-battery-charger.ino
@@ -17,31 +17,35 @@
 
 #include <ESP8266WiFi.h>
 #include "secrets.h"
-#include "ThingSpeak.h" // always include thingspeak header file after other header files and custom macros
+#include "ThingSpeak.h"         // always include thingspeak header file after other header files and custom macros
 
-#define NUM_SAMPLES 10   // number of analog samples to take per reading
-#define BAUD_RATE 115200 // baud rate used for Serial console
-#define ANALOG_PIN_NO A0 // analog pin number
-#define R1 1000000       // resistor 1 on the voltage divider
-#define R2 220000        // resistor 2 on the voltage divider
-#define VOLTAGE_MAX 4.2  // max voltage of lipo battery
-#define VOLTAGE_MIN 3.14 // min voltage of lipo battery
-#define DELAY_LOOP 20000 // delay for loop routine
-#define DELAY_SAMPLE 10  // delay between samples
-#define SLEEP_TIME 60    // sleep time (s)
+#define NUM_SAMPLES 10          // number of analog samples to take per reading
+#define BAUD_RATE 115200        // baud rate used for Serial console
+#define ANALOG_PIN_NO A0        // analog pin number
+#define R1 1000000              // resistor 1 on the voltage divider
+#define R2 220000               // resistor 2 on the voltage divider
+#define VOLTAGE_MAX 4.2         // max voltage of lipo battery
+#define VOLTAGE_MIN 3.14        // min voltage of lipo battery
+//#define DELAY_LOOP 20000      // delay for loop routine
+#define DELAY_SAMPLE 10         // delay between samples
+#define DELAY_WIFI 5            // delay between samples (s)
+#define SLEEP_TIME 60           // sleep time (s)
+#define FIELD_NO_PERCENTAGE 1   // field number of battery percentage
+#define FIELD_NO_LEVEL 2        // field number of battery percentage
 
 WiFiClient  client;
 
-char ssid[] = SECRET_SSID;   // your network SSID (name)
-char pass[] = SECRET_PASS;   // your network password
+char ssid[] = SECRET_SSID;      // your network SSID (name)
+char pass[] = SECRET_PASS;      // your network password
 
 unsigned long myChannelNumber = SECRET_CH_ID;
 const char * myWriteAPIKey = SECRET_WRITE_APIKEY;
 
-int number = 0;
+//int number = 0;
 int sum = 0;                    // sum of samples taken
 unsigned char sample_count = 0; // current sample number
 int level = 0;                  // calculated battery level
+int percentage = 0;             // calculated battery level
 int battery_min = 0;            // min battery level
 int battery_max = 0;            // max battery level
 float resistor_ratio = 0;       // resistor ratio
@@ -64,6 +68,7 @@ void setup() {
 
   WiFi.mode(WIFI_STA);
   ThingSpeak.begin(client);  // Initialize ThingSpeak
+
   Serial.println();
 }
 
@@ -76,12 +81,13 @@ void loop() {
 
   level = getBatteryLevel(sum);
 
-  writeToThingSpeak(level);
+  percentage = getBatteryPercentage(level);
+
+  writeToThingSpeak(percentage, level);
 
   digitalWrite(LED_BUILTIN, HIGH);
-  //delay(DELAY_LOOP);
-  Serial.println("Going to sleep");
-  ESP.deepSleep(SLEEP_TIME * 1e6);
+
+  goToSleep();
 }
 
 void conntectToWifi(){
@@ -93,14 +99,14 @@ void conntectToWifi(){
       WiFi.begin(ssid, pass);  // Connect to WPA/WPA2 network. Change this line if using open or WEP network
       Serial.print(".");
       digitalWrite(LED_BUILTIN, !digitalRead(LED_BUILTIN));
-      delay(5000);
+      delay(DELAY_WIFI*1e3);
     }
     Serial.println("connected");
   }
 }
 
 int getSum(){
-   // take a number of analog samples and add them up
+  // take a number of analog samples and add them up
   while (sample_count < NUM_SAMPLES) {
     sum += analogRead(ANALOG_PIN_NO);
     sample_count++;
@@ -110,23 +116,41 @@ int getSum(){
 }
 
 int getBatteryLevel(int sum){
+  // calculate the average level
   level = (float)sum / (float)NUM_SAMPLES;
-  // convert battery level to percent
-  level = map(level, battery_min, battery_max, 0, 100);
   Serial.print("Battery level: ");
-  Serial.print(level);
-  Serial.println("%");
+  Serial.println(level);
   return level;
 }
 
-void writeToThingSpeak(int level){
+int getBatteryPercentage(int level){
+  // convert battery level to percent
+  percentage = map(level, battery_min, battery_max, 0, 100);
+  Serial.print("Battery percentage: ");
+  Serial.print(percentage);
+  Serial.println("%");
+  return percentage;
+}
+
+void writeToThingSpeak(int percentage, int level){
   // Write to ThingSpeak. There are up to 8 fields in a channel, allowing you to store up to 8 different
   // pieces of information in a channel.  Here, we write to field 1.
-  int x = ThingSpeak.writeField(myChannelNumber, 1, level, myWriteAPIKey);
-  if(x == 200){
+  ThingSpeak.setField(FIELD_NO_PERCENTAGE, percentage);
+  ThingSpeak.setField(FIELD_NO_LEVEL, level);
+  Serial.print("Channel number: ");
+  Serial.println(myChannelNumber);
+  int x = ThingSpeak.writeFields(myChannelNumber, myWriteAPIKey);
+  if (x == 200){
     Serial.println("Channel update successful");
   }
-  else{
+  else {
     Serial.println("Problem updating channel. HTTP error code " + String(x));
   }
+}
+
+void goToSleep(){
+  Serial.print("Going to sleep for ");
+  Serial.print(SLEEP_TIME);
+  Serial.println(" seconds");
+  ESP.deepSleep(SLEEP_TIME * 1e6);
 }
