bc3fb8f3c620b2ad5c019068ad42e3c8a5fd128a
nιcнolaѕ wιlde <ncwilde43@gmail.com>
2022-01-30 22:07:10 -0800

    Update template.ino

--- test/template/template.ino
+++ test/template/template.ino
@@ -15,14 +15,38 @@
 --------------------------------------------------------------*/
 
 #include <ESP8266WiFi.h>
+#include <Ticker.h> 
 #include "secrets.h"
 
-#define BAUD_RATE 115200 // baud rate used for Serial console
-#define DELAY_LOOP 1000  // loop delay time (ms)
-#define ANALOG_PIN_NO A0 // analog pin number
+#define BAUD_RATE 115200        // baud rate used for Serial console
+#define DELAY_LOOP 1000         // loop delay time (ms)
+#define ANALOG_PIN_NO A0        // analog pin number
+#define DELAY_WIFI 5            // delay between samples (s)
+#define INTERVAL_BLINK 100      // blink interval (ms)
+
+WiFiClient  client;
+Ticker blinker;
+
+char ssid[] = SECRET_SSID;      // your network SSID (name)
+char pass[] = SECRET_PASS;      // your network password
+
+unsigned long myChannelNumber = SECRET_CH_ID;
+const char * myWriteAPIKey = SECRET_WRITE_APIKEY;
+const char * myHostName = SECRET_HOSTNAME;
+
+unsigned long previousMillis = 0;    // will store last time LED was updated
+const long interval = 100;           // interval at which to blink (milliseconds)
 
 void setup() {
   Serial.begin(BAUD_RATE);
+  
+  WiFi.mode(WIFI_STA);
+
+  WiFi.hostname(myHostName);
+  
+  blinker.attach_ms(INTERVAL_BLINK, changeState);
+
+  Serial.println();
 }
 
 void loop() {
@@ -30,4 +54,8 @@ void loop() {
   int val = analogRead(ANALOG_PIN_NO)
   Serial.println(val);
   delay(DELAY_LOOP);
+}
+
+void changeState(){
+  digitalWrite(LED_BUILTIN, !(digitalRead(LED_BUILTIN)));
 }
