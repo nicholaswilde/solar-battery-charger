6feb5ddf1b3e10f1bd4e8a5d777965a5ec05ca33
nιcнolaѕ wιlde <ncwilde43@gmail.com>
2022-01-30 16:00:48 -0800

    Add Task badge and eeprom test

---
+++ test/eeprom/README.md
@@ -0,0 +1,52 @@
+# eeprom
+
+Stores values read from analog input 0 into
+the EEPROM. These values will stay in the
+EEPROM when the board is turned off and may
+be retrieved later by another sketch.
+
+## Hypothesis
+
+WIP
+
+## Assumptions
+
+WIP
+
+## Procedure
+
+WIP
+
+### Circuit
+
+Same circuit as [battery](../battery).
+
+![](../battery/images/circuit.png)
+
+Circuit made with [Circuit Diagram](https://www.circuit-diagram.org/)
+
+### Code
+
+WIP
+
+### Output
+
+WIP
+
+## Analysis
+
+WIP
+
+## Conclusion
+
+WIP
+
+## Troubleshooting
+
+WIP
+
+## References
+- https://github.com/esp8266/Arduino/blob/9f536e68f8f3aa3ffae26493dd0a31e1957d2e81/libraries/EEPROM/examples/eeprom_write/eeprom_write.ino
+- https://github.com/esp8266/Arduino/blob/9f536e68f8f3aa3ffae26493dd0a31e1957d2e81/libraries/EEPROM/examples/eeprom_read/eeprom_read.ino
+- https://arduino.stackexchange.com/a/33363
+- https://forum.arduino.cc/t/two-newbie-questions-status-and-printing-a-boolean/77672/3
---
+++ test/eeprom/eeprom.ino
@@ -0,0 +1,53 @@
+/*--------------------------------------------------------------
+  Program:      eeprom
+
+  Description:  Stores values read from analog input 0 into
+                the EEPROM. These values will stay in the
+                EEPROM when the board is turned off and may
+                be retrieved later by another sketch.
+
+  Hardware:     Adafruit Feather Huzzah
+
+  Software:     Developed using arduino-cli 0.20.2.
+
+  Date:         30JAN2022
+
+  Author:       Nicholas Wilde 0x08b7d7a3
+--------------------------------------------------------------*/
+
+#include <EEPROM.h>
+
+#define BAUD_RATE 115200 // baud rate used for Serial console
+#define DELAY_LOOP 1000  // loop delay time (ms)
+#define SLEEP_TIME 10    // sleep time (s)
+#define ANALOG_PIN_NO A0 // analog pin number
+
+int addr = 0;
+byte value;
+
+void setup() {
+  Serial.begin(BAUD_RATE);
+  EEPROM.begin(512);
+
+  value = EEPROM.read(addr);
+  Serial.println();
+  Serial.println("Setup");
+  Serial.print(addr);
+  Serial.print("\t");
+  Serial.print(value, DEC);
+}
+
+void loop() {
+  Serial.println("eeprom");
+  // divide by 4 becasue analog pin range is 0-1023 and byte is 0-255
+  int val = analogRead(ANALOG_PIN_NO) / 4;
+  EEPROM.write(addr, val);
+  Serial.print("Pin value: ");
+  Serial.println(val);
+  if (EEPROM.commit()) {
+    Serial.println("EEPROM successfully committed");
+  } else {
+    Serial.println("ERROR! EEPROM commit failed");
+  }
+  ESP.deepSleep(SLEEP_TIME * 1000000);
+}
