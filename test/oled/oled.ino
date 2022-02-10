51d08981f6e41cdfe52a09b9e493d0a69be5816e
nιcнolaѕ wιlde <ncwilde43@gmail.com>
2022-02-09 21:55:13 -0800

    Add wifi connection

--- test/oled/oled.ino
+++ test/oled/oled.ino
@@ -18,8 +18,15 @@
 #include <Wire.h>
 #include <Adafruit_GFX.h>
 #include <Adafruit_SH110X.h>
+#include <ESP8266WiFi.h>
+#include <Ticker.h>
+#include "secrets.h"
 
-Adafruit_SH1107 display = Adafruit_SH1107(64, 128, &Wire);
+#define BAUD_RATE 115200        // baud rate used for Serial console
+#define DELAY_LOOP 1000         // loop delay time (ms)
+#define ANALOG_PIN_NO A0        // analog pin number
+#define DELAY_WIFI 5            // delay between samples (s)
+#define INTERVAL_BLINK 100      // blink interval (ms)
 
 // OLED FeatherWing buttons map to different pins depending on board:
 #if defined(ESP8266)
@@ -48,49 +55,95 @@ Adafruit_SH1107 display = Adafruit_SH1107(64, 128, &Wire);
   #define BUTTON_C  5
 #endif
 
+const char ssid[] = SECRET_SSID;      // your network SSID (name)
+const char pass[] = SECRET_PASS;      // your network password
+const int ledPin = LED_BUILTIN;
+
+const char * myHostName = SECRET_HOSTNAME;
+
+WiFiClient  client;
+Ticker blinker;
+Adafruit_SH1107 display = Adafruit_SH1107(64, 128, &Wire);
+
 void setup() {
-  Serial.begin(115200);
+  Serial.begin(BAUD_RATE);
   while(!Serial);
   delay(100);
   Serial.println();
-  Serial.println("128x64 OLED FeatherWing test");
-  display.begin(0x3C, true); // Address 0x3C default
+  pinMode(ledPin, OUTPUT);
+  setupDisplay();
+  Serial.println("test: oled");
+  display.println("test: oled");
+  conntectToWifi();
+  display.display();
+}
 
-  Serial.println("OLED begun");
+void loop() {
+  delay(10);
+  yield();
+  display.display();
+}
+
+void print(char* value){
+  Serial.print(value);
+  display.print(value);
+}
+
+void println(char* value){
+  Serial.println(value);
+  display.print(value);
+}
+
+void setupDisplay(){
+  display.begin(0x3C, true); // Address 0x3C default
 
   // Show image buffer on the display hardware.
   // Since the buffer is intialized with an Adafruit splashscreen
   // internally, this will display the splashscreen.
-  display.display();
-  delay(1000);
+  //display.display();
+  //delay(1000);
 
   // Clear the buffer.
   display.clearDisplay();
   display.display();
 
   display.setRotation(1);
-  Serial.println("Button test");
-
-  pinMode(BUTTON_A, INPUT_PULLUP);
-  pinMode(BUTTON_B, INPUT_PULLUP);
-  pinMode(BUTTON_C, INPUT_PULLUP);
-
-  // text display tests
   display.setTextSize(1);
   display.setTextColor(SH110X_WHITE);
   display.setCursor(0,0);
-  display.print("Connecting to SSID\n'adafruit':");
-  display.print("connected!");
-  display.println("IP: 10.0.1.23");
-  display.println("Sending val #0");
-  display.display(); // actually display all of the above
 }
 
-void loop() {
-  if(!digitalRead(BUTTON_A)) display.print("A");
-  if(!digitalRead(BUTTON_B)) display.print("B");
-  if(!digitalRead(BUTTON_C)) display.print("C");
-  delay(10);
-  yield();
+void conntectToWifi(){
+  // Connect or reconnect to WiFi
+  WiFi.mode(WIFI_STA);
+  WiFi.hostname(myHostName);
+  Serial.print("SSID: ");
+  display.print("SSID: ");
+  Serial.println(SECRET_SSID);
+  display.println(SECRET_SSID);
+  Serial.print("Connecting");
+  display.print("Connecting");
+  display.display();
+  while(WiFi.status() != WL_CONNECTED){
+    WiFi.begin(ssid, pass);
+    Serial.print(".");
+    display.print(".");
+    display.display();
+    delay(DELAY_WIFI*1e3);
+  }
+  blinker.detach();
+  Serial.println();
+  display.println();
+  Serial.println("Connected!");
+  display.println("Connected!");
+  display.display();
+  Serial.print("IP: ");
+  display.print("IP: ");
+  Serial.println(WiFi.localIP());
+  display.println(WiFi.localIP());
+  Serial.print("Hostname: ");
+  display.print("Hostname: ");
+  Serial.println(WiFi.hostname());
+  display.println(WiFi.hostname());
   display.display();
 }
